5eae5f006b3905585513e95f650eb02bce0b34d9
Ardish4 <ardishbose@gmail.com>
2026-02-19 12:59:38 +0530

    Refactor HeroNav component layout and improve styling

--- frontend/src/components/HeroNav.tsx
+++ frontend/src/components/HeroNav.tsx
@@ -1,10 +1,17 @@
 import { ModeToggle } from "./mode-toggle"
+import { Button } from "./ui/button"
 
 const HeroNav = () => {
   return (
-    <nav className="flex items-center justify-between p-4">
-      <h1 className="text-xl">AsmissionPRO</h1>
-      <ModeToggle />
+    <nav className="container mx-auto border-b border-dashed">
+      <div className="flex items-center justify-between p-4">
+        <h1 className="text-xl">Admission<span className="text-primary">PRO</span></h1>
+        <div className="flex justify-center items-center gap-2">
+          <ModeToggle />
+          <Button variant="outline" size="sm">Log In</Button>
+          <Button variant="default" size="sm">Sign Up</Button>
+        </div>
+      </div>
     </nav>
   )
 }
