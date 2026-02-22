94cd692d2408365fe1cbf0a86c7992b68f83f2e6
SAYANui <sayans2005@gmail.com>
2026-02-22 15:49:34 +0530

    Added react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router";
 
 import Hero from "@/pages/hero";
+import StudentLogin from "./pages/student/StudentLogin";
+import StudentSignup from "./pages/student/StudentSignup";
 import { ThemeProvider } from "./components/theme-provider";
 
 export function App() {
@@ -8,6 +10,8 @@ export function App() {
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Routes>
         <Route path="/" element={<Hero />} />
+        <Route path="/login" element={<StudentLogin />} />
+        <Route path="/signup" element={<StudentSignup />} />
       </Routes>
     </ThemeProvider>
   )
