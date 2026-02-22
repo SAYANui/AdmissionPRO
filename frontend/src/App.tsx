2d1f054435b37818e10aede7f7cefc183d83cd0f
Ardish4 <ardishbose@gmail.com>
2026-02-22 17:17:08 +0530

    Add AdminDashboard component and update routing in App; refactor HeroNav structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router";
 
-import Hero from "@/pages/Hero";
 import { ThemeProvider } from "./components/theme-provider";
+import Hero from "@/pages/Hero";
+import AdminDashboard from "./pages/AdminDashboard";
 
 export function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Routes>
         <Route path="/" element={<Hero />} />
+        <Route path="/dashboard" element={<AdminDashboard />} />
       </Routes>
     </ThemeProvider>
   )
--- frontend/src/components/HeroNav.tsx
+++ frontend/src/components/HeroNav.tsx
@@ -6,16 +6,16 @@ const HeroNav = () => {
   const navigate = useNavigate()
 
   return (
-    <nav className="container mx-auto border-b border-dashed">
-      <div className="flex items-center justify-between p-4">
-        <h1 className="text-xl">Admission<span className="text-primary">PRO</span></h1>
+    <header className="container mx-auto border-b border-dashed">
+      <nav className="flex items-center justify-between p-4">
+        <h1 className="text-xl">Admission<span className="text-primary font-extrabold">PRO</span></h1>
         <div className="flex justify-center items-center gap-2">
           <ModeToggle />
           <Button variant="outline" size="sm" onClick={() => navigate("/login")}>Log In</Button>
           <Button variant="default" size="sm" onClick={() => navigate("/signup")}>Sign Up</Button>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </header>
   )
 }
 
